cc6702c7863513897de1fa9bd770eb3d3c0e4283
I Putu Wira Budhi Guna Ariyasa <wirbud1134@gmail.com>
2025-02-06 23:09:18 +0800

    feat: add authentication routes for login, registration, and logout with resource routes for permissions, roles, and users

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,15 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/login', [AuthenticatedSessionController::class, 'store']);
+Route::post('/register', [RegisteredUserController::class, 'store']);
+Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->middleware('auth:sanctum');
+
+Route::middleware('auth:sanctum')->group(function () {
+    // Permissions
+    Route::apiResource('permissions', PermissionController::class);
+    // Roles
+    Route::apiResource('roles', RoleController::class);
+    // Users
+    Route::apiResource('users', UserController::class);
 });
