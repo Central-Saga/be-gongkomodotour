936d7979f8e2cd18f8f8d843643bea40e9e020c6
I Putu Wira Budhi Guna Ariyasa <wirbud1134@gmail.com>
2025-02-06 23:39:09 +0800

    feat: add HasApiTokens trait to User model and update API routes for authentication and role management

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\PermissionController;
+use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
 
 Route::post('/login', [AuthenticatedSessionController::class, 'store']);
 Route::post('/register', [RegisteredUserController::class, 'store']);
