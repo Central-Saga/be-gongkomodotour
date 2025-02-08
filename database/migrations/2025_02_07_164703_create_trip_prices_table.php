e19548a52762c00d59ec6756a52b28bf2f9af091
I Putu Wira Budhi Guna Ariyasa <wirbud1134@gmail.com>
2025-02-08 14:22:20 +0800

    feat: add TripPrices model, migration, factory, and seeder with relationship to TripDuration

---
+++ database/migrations/2025_02_07_164703_create_trip_prices_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trip_prices', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('trip_duration_id')->constrained('trip_durations')->onDelete('cascade');
+            $table->integer('pax_min');
+            $table->integer('pax_max');
+            $table->decimal('price_per_pax', 10, 2);
+            $table->enum('status', ['Aktif', 'Non Aktif'])->default('Aktif');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trip_prices');
+    }
+};
