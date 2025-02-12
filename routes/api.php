e73619ad928563746f8e86efb33d7d0cbb9702c9
I Putu Wira Budhi Guna Ariyasa <wirbud1134@gmail.com>
2025-02-13 00:48:05 +0800

    feat: add API routes for itineraries, flight schedules, trips, trip prices, and trip durations

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,11 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\PermissionController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\ItinerariesController;
+use App\Http\Controllers\FlightScheduleController;
+use App\Http\Controllers\TripController;
+use App\Http\Controllers\TripPricesController;
+use App\Http\Controllers\TripDurationController;
 
 Route::post('/login', [AuthenticatedSessionController::class, 'store']);
 Route::post('/register', [RegisteredUserController::class, 'store']);
@@ -19,4 +24,14 @@ Route::middleware('auth:sanctum', 'check.user.status')->group(function () {
     Route::apiResource('roles', RoleController::class);
     // Users
     Route::apiResource('users', UserController::class);
+    // Itineraries
+    Route::apiResource('itineraries', ItinerariesController::class);
+    // Flight Schedules
+    Route::apiResource('flight-schedules', FlightScheduleController::class);
+    // Trips
+    Route::apiResource('trips', TripController::class);
+    // Trip Prices
+    Route::apiResource('trip-prices', TripPricesController::class);
+    // Trip Durations
+    Route::apiResource('trip-durations', TripDurationController::class);
 });
