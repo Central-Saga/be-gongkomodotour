9d7b33dec75ee4577aa31a63734d8dafed4e2e09
Borbobb <madeadisujaya23@gmail.com>
2025-02-12 19:39:17 +0800

    update Email

--- database/migrations/2025_02_12_051715_create_email_blast_recipients_table.php
+++ database/migrations/2025_02_12_051715_create_email_blast_recipients_table.php
@@ -19,6 +19,6 @@ return new class extends Migration
     
     public function down(): void
     {
-        Schema::dropIfExists('email_blast_recipients');
+        Schema::dropIfExists('email_blast_recipient');
     }
 };
