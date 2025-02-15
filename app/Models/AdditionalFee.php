7d2b7332044ec81df9af008d398fe67789640e8d
I Putu Wira Budhi Guna Ariyasa <wirbud1134@gmail.com>
2025-02-15 12:42:33 +0800

    feat: create model, migration, seeder, and factory for Booking, Additional Fee, Surcharge, Booking Fee

---
+++ app/Models/AdditionalFee.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AdditionalFee extends Model
+{
+    /** @use HasFactory<\Database\Factories\AdditionalFeeFactory> */
+    use HasFactory;
+}
