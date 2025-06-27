5ecf1e2c29f1ab789b2d6d54a5a608906d84c1c0
I Putu Wira Budhi Guna Ariyasa <wirbud1134@gmail.com>
2025-06-27 14:57:54 +0800

    refactor: Simplify testimonial repository by removing unnecessary customer and user relationships in queries

--- app/Repositories/Eloquent/TestimonialRepository.php
+++ app/Repositories/Eloquent/TestimonialRepository.php
@@ -18,18 +18,18 @@ class TestimonialRepository implements TestimonialRepositoryInterface
 
     public function getAllTestimonial()
     {
-        return $this->model->with('customer', 'customer.user', 'trip')->get();
+        return $this->model->with('trip')->get();
     }
 
     public function getTestimonialById($id)
     {
-        return $this->model->with('customer', 'customer.user', 'trip')->find($id);
+        return $this->model->with('trip')->find($id);
     }
 
     public function createTestimonial(array $data)
     {
         $testimonial = $this->model->create($data);
-        $testimonial->load('customer', 'customer.user', 'trip');
+        $testimonial->load('trip');
         return $testimonial;
     }
 
@@ -39,7 +39,7 @@ class TestimonialRepository implements TestimonialRepositoryInterface
 
         if ($testimonial) {
             $testimonial->update($data);
-            $testimonial->load('customer', 'customer.user', 'trip');
+            $testimonial->load('trip');
         }
 
         return $testimonial;
@@ -51,7 +51,6 @@ class TestimonialRepository implements TestimonialRepositoryInterface
 
         if ($testimonial) {
             $testimonial->delete();
-            $testimonial->load('customer', 'customer.user', 'trip');
             return true;
         }
 
@@ -70,12 +69,12 @@ class TestimonialRepository implements TestimonialRepositoryInterface
 
     public function getTestimonialByApproved($approved)
     {
-        return $this->model->where('is_approved', $approved)->with('customer', 'customer.user', 'trip')->get();
+        return $this->model->where('is_approved', $approved)->with('trip')->get();
     }
 
     public function getTestimonialByHighlight($highlight)
     {
-        return $this->model->where('is_highlight', $highlight)->with('customer', 'customer.user', 'trip')->get();
+        return $this->model->where('is_highlight', $highlight)->with('trip')->get();
     }
 
     public function getTestimonialByFilters($approved = null, $highlight = null)
@@ -90,6 +89,6 @@ class TestimonialRepository implements TestimonialRepositoryInterface
             $query->where('is_highlight', (bool) $highlight);
         }
 
-        return $query->with('customer', 'customer.user', 'trip')->get();
+        return $query->with('trip')->get();
     }
 }
