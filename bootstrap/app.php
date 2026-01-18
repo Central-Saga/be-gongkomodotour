e977de7a5c3cefb824909eb01cb236913c0b310c
I Putu Wira Budhi Guna Ariyasa <wirbud1134@gmail.com>
2026-01-18 20:01:51 +0800

    feat: Add CSRF token validation to middleware for login and registration endpoints

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,6 +18,11 @@ return Application::configure(basePath: dirname(__DIR__))
 
         $middleware->statefulApi();
 
+        $middleware->validateCsrfTokens(except: [
+            'api/login',
+            'api/register',
+        ]);
+
 
         $middleware->alias([
             'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
